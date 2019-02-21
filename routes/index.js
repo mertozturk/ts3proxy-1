534000a69b0235194c264ce1d916486dc3409381
UnlimitedBytes <admin@unlimitedbytes.ovh>
2019-02-21 03:16:42 +0000

    Fixed #5

--- routes/index.js
+++ routes/index.js
@@ -13,84 +13,95 @@ router.post('/', (req, res) => {
 		return res.status(200).render("index");
 	}
 
-	function getIPFromInput(input, cb) {
-		const dns = require('dns');
-		const isIp = require('is-ip');
-
-		let address = input;
-		let port = 9987;
-		let customport = false;
-		if(input.indexOf(":") > -1) {
-			address = input.split(":")[0];
-			port = input.split(":")[1];
-			customport = true;
-		}
-
-		if(port < 1 || port > 65535) {
-			return console.error("Port invalid.");
-		}
-
-		if(!isIp.v4(address)) {
-			dns.resolveSrv("_ts3._udp." + address, (err, srvs) => {
-				if(err) {
-					dns.resolve4(address, (err, ips) => {
-						if(err) {
-							return cb(new Error("Invalid address."));
-						}
-
-						address = ips[0];
-						return cb(null, address, port);
-					});
-				} else {
-					dns.resolve4(srvs[0].name, (err, ips) => {
-						if(err) {
-							return cb(new Error("Invalid address."));
-						}
-
-						address = ips[0];
-						port = customport ? port : srvs[0].port;
-						return cb(null, address, port);
-					});
-				}
-			});
-		} else {
-			return cb(null, address, port);
-		}
-	}
-	
 	getIPFromInput(req.body.ip, (err, address, port) => {
 		if(err) return res.render("index", {
 			error: err.name
 		});
 
-		const udpproxy = require('udp-proxy');
-		let proxyport = -1;
-		for(let i = 0; i < 5; i++) {
-			try {
-				let tempproxyport = Math.floor(Math.random() * (config.ports.to - config.ports.from)) + config.ports.from;
-				const server = udpproxy.createServer({
-					address: address,
-					port: port,
-					localport: tempproxyport
-				});
-				proxyport = tempproxyport;
-				break;
-			} catch(e) {
-				console.error(e);
-			}
-		}
+		createProxy(address, port, (err, proxyport) => {
+			if(err) return res.render("index", {
+				error: "Could not find a free port. Please try again."
+			});
 
-		if(proxyport == -1) return res.render("index", {
-			error: "Could not find a free port. Please try again."
-		});
-
-		res.status(200).render("index", {
-			proxy: {
-				ip: config.ipaddress,
-				port: proxyport
-			}
+			res.status(200).render("index", {
+				proxy: {
+					ip: config.ipaddress,
+					port: proxyport
+				}
+			});
 		});
 	});
 });
+
+function getIPFromInput(input, cb) {
+	const dns = require('dns');
+	const isIp = require('is-ip');
+
+	let address = input;
+	let port = 9987;
+	let customport = false;
+	if(input.indexOf(":") > -1) {
+		address = input.split(":")[0];
+		port = input.split(":")[1];
+		customport = true;
+	}
+
+	if(port < 1 || port > 65535) {
+		return console.error("Port invalid.");
+	}
+
+	if(!isIp.v4(address)) {
+		dns.resolveSrv("_ts3._udp." + address, (err, srvs) => {
+			if(err) {
+				dns.resolve4(address, (err, ips) => {
+					if(err) {
+						return cb(new Error("Invalid address."));
+					}
+
+					address = ips[0];
+					return cb(null, address, port);
+				});
+			} else {
+				dns.resolve4(srvs[0].name, (err, ips) => {
+					if(err) {
+						return cb(new Error("Invalid address."));
+					}
+
+					address = ips[0];
+					port = customport ? port : srvs[0].port;
+					return cb(null, address, port);
+				});
+			}
+		});
+	} else {
+		return cb(null, address, port);
+	}
+}
+
+function createProxy(address, port, cb, round) {
+	if(!round) round = 1;
+	if(round >= 5) return cb(new Error('Could not find a free port. Please try again.'));
+
+	let tempproxyport = Math.floor(Math.random() * (config.ports.to - config.ports.from)) + config.ports.from;
+	
+	const udpproxy = require('udp-proxy');
+	const server = udpproxy.createServer({
+		address: address,
+		port: port,
+		localport: tempproxyport
+	});
+
+	server.on('proxyError', function (err) {
+		createProxy(address, port, cb, ++round);
+	});
+
+	server.on('error', function (err) {
+		createProxy(address, port, cb, ++round);
+	});
+
+	server.on('listening', function (err) {
+		cb(null, tempproxyport);
+	});
+}
 
 module.exports = router;
